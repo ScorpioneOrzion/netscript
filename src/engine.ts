294376fdd09486c36ad952d6c60cc7ffbe18d2df
ScorpioneOrzion <36512390+ScorpioneOrzion@users.noreply.github.com>
2024-08-22 10:50:45 +0200

    rewrote

--- src/engine.ts
+++ src/engine.ts
@@ -1,105 +1,70 @@
-
-// Define primitive types
+// Declare types
 export type Primitive = number | string | boolean;
-
-// Define an object with primitive values or other PrimitiveObjects
-export type PrimitiveObject = { [key: string]: Primitive | PrimitiveObject; };
-
-// Define the target object type with optional parent scope
-export type ScopeObject = PrimitiveObject & defaultObject;
-
-const PARENT_SCOPE_SYMBOL = Symbol("parentScope");
-const CONSTANT_SYMBOL = Symbol("constant")
-const VARIABLE_SYMBOL = Symbol("variable")
-// Proxy handler for scopes
-const scopeHandler: ProxyHandler<ScopeObject> = {
-	get(target, prop: string, receiver) {
-		if (prop in target) {
-			return target[prop];
-		}
-		if (target[PARENT_SCOPE_SYMBOL]) {
-			return Reflect.get(target[PARENT_SCOPE_SYMBOL], prop, receiver);
-		}
-		throw new Error(`Variable '${prop}' is not defined in the current scope.`);
-	},
-	set(target, prop: string, value) {
-		target[prop] = value;
-		return true;
-	},
-	deleteProperty() {
-		return false
-	}
-};
-
-// Proxy handler for Objects
-const objectHandler: ProxyHandler<PrimitiveObject> = {
-	get(target, prop: string, _reciever) {
-		if (prop === 'constructor') {
-			return target[prop];
-		}
-		if (prop in target) {
-			const value = target[prop];
-			if (typeof value === 'object' && value !== null) {
-				return new Proxy(value, objectHandler);
-			}
-			return value;
-		}
-		throw new Error(`Property '${prop}' is not defined in the current scope.`);
-	},
-	set(target, prop: string, value) {
-		if (typeof value === 'object' && value !== null) {
-			value = new Proxy(value, objectHandler);
-		}
-		target[prop] = value;
-		return true;
-	},
-	deleteProperty(target, prop: string) {
-		return delete target[prop];
-	}
-};
-
-type defaultObject = {
-	[PARENT_SCOPE_SYMBOL]?: ScopeObject
+export type ObjectType = {
+	[key: string]: Primitive | ObjectType | FunctionType | undefined
+	[TYPE_SCOPE]: typeof OBJECT_TYPE
+}
+export type FunctionType = {
+	func: () => Primitive | ObjectType | FunctionType | undefined
+	argNames: string[]
+	[TYPE_SCOPE]: typeof FUNCTION_TYPE
+}
+export type ScopeType = {
+	[key: string]: Primitive | ObjectType | FunctionType | undefined
+	[PARENT_SCOPE_SYMBOL]?: ScopeType
 	[CONSTANT_SYMBOL]: Set<string>
 	[VARIABLE_SYMBOL]: Set<string>
+	[TYPE_SCOPE]: typeof SCOPE_TYPE
+}
+type ChildScopeType = ScopeType & {
+	[PARENT_SCOPE_SYMBOL]: ScopeType
 }
 
-function defaultObject(): ScopeObject {
+// Declare consts
+const PARENT_SCOPE_SYMBOL = Symbol("parentScope");
+const CONSTANT_SYMBOL = Symbol("constant");
+const VARIABLE_SYMBOL = Symbol("variable");
+const FUNCTION_TYPE = Symbol("function");
+const OBJECT_TYPE = Symbol("object");
+const SCOPE_TYPE = Symbol("scope");
+const TYPE_SCOPE = Symbol("type_scope");
+const reservedKeywords = new Set(['function', 'const', 'let', 'undefined']);
+function defaultObject(): ScopeType {
 	return {
 		[CONSTANT_SYMBOL]: new Set(),
-		[VARIABLE_SYMBOL]: new Set()
-	}
+		[VARIABLE_SYMBOL]: new Set(),
+		[TYPE_SCOPE]: SCOPE_TYPE,
+	};
 }
 
-// Initialize scope stack with global scope
-const scopeStack = [new Proxy(defaultObject(), scopeHandler)];
+const scopeStack = [defaultObject()]
 
-// Enter a new scope
 function enterScope() {
 	const parentScope = scopeStack[scopeStack.length - 1];
-	const newScope = new Proxy(Object.assign(defaultObject(), { [PARENT_SCOPE_SYMBOL]: parentScope }), scopeHandler);
+	const newScope = Object.assign(defaultObject(), { [PARENT_SCOPE_SYMBOL]: parentScope });
 	scopeStack.push(newScope);
 }
 
-// Exit the current scope
 function exitScope() {
 	if (scopeStack.length === 1) throw new Error(`Cannot exit the global scope.`);
 	scopeStack.pop();
 }
 
-// Declare a variable in the current scope
-function declareVariable(varName: string, value: Primitive | PrimitiveObject) {
+function declareVariable(varName: string, value: FunctionType | Primitive | ObjectType | undefined) {
 	const currentScope = scopeStack[scopeStack.length - 1];
-	if (currentScope[CONSTANT_SYMBOL].has(varName)) {
+	if (reservedKeywords.has(varName))
+		throw new Error(`Cannot redefine reserved keyword '${varName}'.`);
+	if (currentScope[CONSTANT_SYMBOL].has(varName))
 		throw new Error(`Can't redefine constant '${varName}'`);
-	}
+
 	currentScope[VARIABLE_SYMBOL].add(varName)
 	SetValue(currentScope, varName, value)
 }
 
-// Declare a const in the current scope
-function declareConst(varName: string, value: Primitive | PrimitiveObject) {
+function declareConst(varName: string, value: FunctionType | Primitive | ObjectType | undefined) {
 	const currentScope = scopeStack[scopeStack.length - 1];
+	if (reservedKeywords.has(varName))
+		throw new Error(`Cannot redefine reserved keyword '${varName}'.`);
 	if (currentScope[CONSTANT_SYMBOL].has(varName)) {
 		throw new Error(`Can't redefine constant '${varName}'`);
 	}
@@ -110,18 +75,50 @@ function declareConst(varName: string, value: Primitive | PrimitiveObject) {
 	SetValue(currentScope, varName, value)
 }
 
-function SetValue(currentScope: ScopeObject, varName: string, value: Primitive | PrimitiveObject) {
-	if (typeof value === "object") {
-		currentScope[varName] = new Proxy(value, objectHandler);
-	} else {
-		currentScope[varName] = value;
-	}
+function SetValue(currentScope: ScopeType, varName: string, value: FunctionType | ObjectType | Primitive | undefined) {
+	currentScope[varName] = value
 }
 
-// Get a variable from the current scope
+function hasParent(scope: ScopeType): scope is ChildScopeType {
+	return PARENT_SCOPE_SYMBOL in scope
+}
+
+function executeFunction(fn: FunctionType, args: (FunctionType | ObjectType | Primitive | undefined)[]) {
+	enterScope()
+	for (let i = 0; i < fn.argNames.length; i++) {
+		declareVariable(fn.argNames[i], args[i]);
+	}
+	const result = fn.func()
+	exitScope();
+	return result;
+}
+
+function declareProperty(object: ObjectType, varName: string, value: FunctionType | Primitive | ObjectType | undefined) {
+	if (reservedKeywords.has(varName))
+		throw new Error(`Cannot redefine reserved keyword '${varName}'.`);
+
+	object[varName] = value
+}
+
+function deleteProperty(object: ObjectType, propName: string) {
+	if (propName in object) return delete object[propName]
+	return false
+}
+
+function getProperty(object: ObjectType, propName: string) {
+	if (propName in object) return object[propName]
+	throw new Error(`Property '${propName}' is not defined.`);
+}
+
 function getVariable(varName: string) {
-	const currentScope = scopeStack[scopeStack.length - 1];
-	return currentScope[varName];
+	let currentScope = scopeStack[scopeStack.length - 1]
+	while (true) {
+		if (varName in currentScope) return currentScope[varName];
+		if (hasParent(currentScope))
+			currentScope = currentScope[PARENT_SCOPE_SYMBOL]
+		else
+			throw new Error(`Variable '${varName}' is not defined in the current scope.`);
+	}
 }
 
 export default () => {
@@ -129,8 +126,15 @@ export default () => {
 		scopeStack,
 		enterScope,
 		exitScope,
+
 		declareVariable,
 		declareConst,
-		getVariable
+		getVariable,
+
+		executeFunction,
+
+		declareProperty,
+		deleteProperty,
+		getProperty
 	};
 };
